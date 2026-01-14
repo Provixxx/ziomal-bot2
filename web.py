d1c2daeec7eb3527415864d450ba3e06ad31d04f
Provixxx <bartoszguzniczak1998@gmail.com>
2026-01-14 18:40:53 +0100

    Create web.py

---
+++ web.py
@@ -0,0 +1,14 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PORT = int(os.environ.get("PORT", 8000))
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    server.serve_forever()
